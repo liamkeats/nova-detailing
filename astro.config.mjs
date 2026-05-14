23ee5a28a06d0b0717128b80034305b52bb5c65f
liamkeats <keatsliam@gmail.com>
2026-05-13 23:29:10 -0300

    made CMS work live again for sanity posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   image: {
@@ -17,4 +19,6 @@ export default defineConfig({
       allowedHosts: true,
     },
   },
+
+  adapter: netlify(),
 });
